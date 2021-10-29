aff9219ca770407af43f5d7e07fe78268365307d
yule.zhang <zhangyule1993@sina.com>
2021-10-29 11:21:06 +0800

    下载功能

--- src/main/java/com/biubiu/weixinapp/controller/VideoController.java
+++ src/main/java/com/biubiu/weixinapp/controller/VideoController.java
@@ -10,12 +10,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 /**
@@ -39,7 +37,7 @@ public class VideoController {
     @PostMapping(value = "analysis")
     public ResponseEntity<?> analysis(@RequestBody AnalysisDTO analysisDTO) {
         if(analysisDTO == null || StringUtils.isBlank(analysisDTO.getCode()) || StringUtils.isBlank(analysisDTO.getText())) {
-            return new ResponseEntity<>("参数错误", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("[VideoController] - [analysis] - 参数错误", HttpStatus.BAD_REQUEST);
         }
         try {
             Map<String, String> map = videoService.analysis(analysisDTO);
@@ -48,6 +46,21 @@ public class VideoController {
             logger.error("", e);
         }
         return new ResponseEntity<>("服务异常", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @SystemLog(description = "下载网络视频")
+    @GetMapping(value = "download")
+    public void download(@RequestParam("playUrl") String playUrl, HttpServletResponse response) {
+        if(StringUtils.isBlank(playUrl)) {
+            logger.error("[VideoController] - [download] - 视频播放地址不能为空");
+            throw new NullPointerException("[VideoController] - [download] - 视频播放地址不能为空");
+        }
+        try {
+            videoService.download(playUrl, response);
+        } catch(Exception e) {
+            logger.error("[VideoController] - [download] - 解析网络视频失败 - {}", e.getMessage());
+        }
     }
 
 }
--- src/main/java/com/biubiu/weixinapp/service/VideoService.java
+++ src/main/java/com/biubiu/weixinapp/service/VideoService.java
@@ -6,9 +6,19 @@ import com.biubiu.weixinapp.enums.ChannelType;
 import com.biubiu.weixinapp.model.DouYinModel;
 import com.biubiu.weixinapp.utils.AnalysisUtils;
 import com.google.common.collect.ImmutableMap;
+import org.jsoup.Jsoup;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * @author ：张音乐
@@ -40,5 +50,25 @@ public class VideoService {
         return ImmutableMap.of("video", addrModel.getVideoAddr(), "cover", addrModel.getCoverAddr());
     }
 
+
+    public void download(String playUrl, HttpServletResponse response) throws IOException {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Connection", "keep-alive");
+        headers.put("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1");
+        BufferedInputStream in = Jsoup.connect(playUrl).headers(headers).timeout(10000).ignoreContentType(true).execute().bodyStream();
+        OutputStream out = response.getOutputStream();
+        // 重置response
+        response.reset();
+        // ContentType，即告诉客户端所发送的数据属于什么类型
+        response.setContentType("application/octet-stream; charset=UTF-8");
+        // 设置编码格式
+        response.setHeader("Content-Disposition", "attachment;fileName=" + UUID.randomUUID().toString().replace("-", "") + ".mp4");
+        int b;
+        while ((b = in.read()) != -1) {
+            out.write(b);
+        }
+        out.close();//关闭输出流
+        in.close(); //关闭输入流
+    }
 
 }
